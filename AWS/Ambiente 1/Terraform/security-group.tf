432aceec6c18ffe32ace95b5dbfff6280baa9f5d
root <cbarbieri@vertigo.com.br>
2022-06-01 10:24:35 -0300

    fix: security group + playbook

--- AWS/Ambiente 1/Terraform/security-group.tf
+++ AWS/Ambiente 1/Terraform/security-group.tf
@@ -10,17 +10,10 @@ resource "aws_security_group" "open_ports" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 53
-    to_port     = 53
-    protocol    = "tcp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
